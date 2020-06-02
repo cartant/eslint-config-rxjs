29ea282ba18dbc38286e3f6f473d4c591f998b0c
Nicholas Jamieson <nicholas@cartant.com>
2020-06-02 20:59:55 +1000

    chore: Configure rules.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 module.exports = {
   rules: {
-    // "no-async-subscribe"
-    // "no-compat"
-    // "no-connectable"
-    // "no-create"
-    // "no-explicit-generics"
-    // "no-ignored-notifier"
-    // "no-ignored-replay-buffer"
-    // "no-ignored-observable"
-    // "no-ignored-subscription"
-    // "no-ignored-takewhile-value"
-    // "no-index"
-    // "no-internal"
-    // "no-nested-subscribe"
-    // "no-sharereplay"
-    // "no-subclass"
-    // "no-subject-unsubscribe"
-    // "no-subject-value"
-    // "no-unbound-methods"
-    // "no-unsafe-catch"
-    // "no-unsafe-first"
-    // "no-unsafe-scope"
-    // "no-unsafe-subject-next"
-    // "no-unsafe-switchmap"
-    // "no-unsafe-takeuntil"
-    // "throw-error"
+    "no-async-subscribe": ["error"],
+    "no-compat": ["error"],
+    "no-connectable": ["error"],
+    "no-create": ["error"],
+    "no-explicit-generics": ["error"],
+    "no-ignored-notifier": ["error"],
+    "no-ignored-replay-buffer": ["error"],
+    "no-ignored-observable": ["error"],
+    "no-ignored-subscription": ["error"],
+    "no-ignored-takewhile-value": ["error"],
+    "no-index": ["error"],
+    "no-internal": ["error"],
+    "no-nested-subscribe": ["error"],
+    "no-sharereplay": ["error", { "allowConfig": true }],
+    "no-subclass": ["error"],
+    "no-subject-unsubscribe": ["error"],
+    "no-subject-value": ["error"],
+    "no-unbound-methods": ["error"],
+    "no-unsafe-catch": ["error"],
+    "no-unsafe-first": ["error"],
+    "no-unsafe-subject-next": ["error"],
+    "no-unsafe-switchmap": ["error"],
+    "no-unsafe-takeuntil": ["error"],
+    "throw-error": ["error"]
   }
 };
